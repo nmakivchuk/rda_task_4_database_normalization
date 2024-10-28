0e2d7a26ef579c283c8c04651deb8167139122cd
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-10-28 19:04:38 +0200

    normalize database

--- task.sql
+++ task.sql
@@ -4,30 +4,33 @@ CREATE DATABASE ShopDB;
 USE ShopDB;
 
 CREATE TABLE Countries (
-    ID INT,
-    Name VARCHAR(50),
-    PRIMARY KEY (ID)
+    ID INT PRIMARY KEY,
+    Name VARCHAR(50)
+);
+
+CREATE TABLE Warehouses (
+    WarehouseID INT PRIMARY KEY,
+    WarehouseName VARCHAR(50),
+    WarehouseAddress VARCHAR(50),
+    CountryID INT,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID)
 );
 
 CREATE TABLE ProductInventory (
-    ID INT,
+    ID INT PRIMARY KEY,
     ProductName VARCHAR(50),
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+    WarehouseID INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID)
 );
 
 -- Populate test data
+INSERT INTO Warehouses (WarehouseID, WarehouseName, WarehouseAddress, CountryID)
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
+INSERT INTO Warehouses (WarehouseID, WarehouseName, WarehouseAddress, CountryID)
+    VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID)
+    VALUES (1, 'AwesomeProduct', 2, 1);
+INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID)
+    VALUES (2, 'AwesomeProduct', 5, 2);
