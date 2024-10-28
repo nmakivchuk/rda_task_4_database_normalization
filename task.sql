7e37247fa2e41d36a13b866bd6217a2747ab9b4c
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-10-28 19:51:32 +0200

    change database structure

--- task.sql
+++ task.sql
@@ -16,21 +16,31 @@ CREATE TABLE Warehouses (
     FOREIGN KEY (CountryID) REFERENCES Countries(ID)
 );
 
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(50) UNIQUE
+);
+
 CREATE TABLE ProductInventory (
     ID INT PRIMARY KEY,
-    ProductName VARCHAR(50),
-    WarehouseAmount INT,
+    ProductID INT,
     WarehouseID INT,
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(WarehouseID)
+    WarehouseAmount INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ProductID),
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(WarehouseID),
+    UNIQUE (ProductID, WarehouseID)
 );
 
 -- Populate test data
-INSERT INTO Warehouses (WarehouseID, WarehouseName, WarehouseAddress, CountryID)
-    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1);
-INSERT INTO Warehouses (WarehouseID, WarehouseName, WarehouseAddress, CountryID)
-    VALUES (2, 'Warehouse-2', 'City-2, Street-2', 2);
+INSERT INTO Countries (ID, Name)
+    VALUES (1, 'Country1'), (2, 'Country2');
 
-INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID)
-    VALUES (1, 'AwesomeProduct', 2, 1);
-INSERT INTO ProductInventory (ID, ProductName, WarehouseAmount, WarehouseID)
-    VALUES (2, 'AwesomeProduct', 5, 2);
+INSERT INTO Warehouses (WarehouseID, WarehouseName, WarehouseAddress, CountryID)
+    VALUES (1, 'Warehouse-1', 'City-1, Street-1', 1),
+           (2, 'Warehouse-2', 'City-2, Street-2', 2);
+
+INSERT INTO Products (ProductID, ProductName)
+    VALUES (1, 'AwesomeProduct');
+
+INSERT INTO ProductInventory (ID, ProductID, WarehouseAmount, WarehouseID)
+    VALUES (1, 1, 2, 1), (2, 1, 5, 2);
